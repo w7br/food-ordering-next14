736801077efad1874114f7cf1c203adebf047d30
Allexus Technology <ale988240769@gmail.com>
2023-11-26 19:55:49 -0300

    forçando upload versão beta x13

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import {UserInfo} from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
 import {User} from '@/models/User';
-import NextAuth, {getServerSession} from "next-auth";
+import NextAuth, {getServerSession, providers} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
