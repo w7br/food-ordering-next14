e21adfb74009ce83be210dc54786eab4ac58b70c
Allexus Technology <ale988240769@gmail.com>
2023-11-27 14:44:34 -0300

    webhook implementado com ia x1

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,29 +1,66 @@
-import {Order} from "@/models/Order";
+import { createServer } from 'https';
+import { parse } from 'url';
+import next from 'next';
+import fs from 'fs';
+import express from 'express';
+import logger from 'morgan';
 
-const stripe = require('stripe')(process.env.STRIPE_SK);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-export async function POST(req) {
-  const sig = req.headers.get('stripe-signature');
-  let event;
+const httpsOptions = {
+  cert: fs.readFileSync(""), // Certificado fullchain do dominio
+  key: fs.readFileSync("/"), // Chave privada do domínio
+  ca: fs.readFileSync(""),   // Certificado público da Efí
+  minVersion: "TLSv1.2",
+  requestCert: true,
+  rejectUnauthorized: true, //Caso precise que os demais endpoints não rejeitem requisições sem mTLS, você pode alterar para false
+};
 
-  try {
-    const reqBuffer = await req.text();
-    const signSecret = process.env.STRIPE_SIGN_SECRET;
-    event = stripe.webhooks.constructEvent(reqBuffer, sig, signSecret);
-  } catch (e) {
-    console.error('stripe error');
-    console.log(e);
-    return Response.json(e, {status: 400});
-  }
+app.prepare().then(() => {
+  const server = express();
 
-  if (event.type === 'checkout.session.completed') {
-    console.log(event);
-    const orderId = event?.data?.object?.metadata?.orderId;
-    const isPaid = event?.data?.object?.payment_status === 'paid';
-    if (isPaid) {
-      await Order.updateOne({_id:orderId}, {paid:true});
+  server.use(logger('dev'));  // Comente essa linha caso não queira que seja exibido o log do servidor no seu console
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
+
+  // Endpoint para configuração do webhook, você precisa cadastrar https://SEUDOMINIO.com/webhook
+  server.post("/webhook", (request, response) => {
+    // Verifica se a requisição que chegou nesse endpoint foi autorizada
+    if (request.socket.authorized) { 
+      response.status(200).end();
+    } else {
+      response.status(401).end();
     }
-  }
+  });
 
-  return Response.json('ok', {status: 200});
-}
+  // Endpoind para recepção do webhook tratando o /pix
+  server.post("/webhook/pix", (request, response) => {
+    if (request.socket.authorized){
+      //Seu código tratando a callback
+      /* EXEMPLO:
+      var body = request.body;
+      filePath = __dirname + "/data.json";
+      fs.appendFile(filePath, JSON.stringify(body) + "\n", function (err) {
+          if (err) {
+              console.log(err);
+          } else {
+              response.status(200).end();
+          }
+      })*/
+      response.status(200).end();
+    }else{
+      response.status(401).end();
+    }
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res, parse(req.url, true));
+  });
+
+  createServer(httpsOptions, server).listen(443, err => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:443');
+  });
+});
