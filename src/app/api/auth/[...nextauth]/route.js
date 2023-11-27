e1121e4244b4b00cab87e69f21aa4c01b8d13f49
Allexus Technology <ale988240769@gmail.com>
2023-11-27 16:47:08 -0300

    webhook implementado com ia x14

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, {getServerSession} from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
