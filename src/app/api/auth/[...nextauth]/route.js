a66644dbee97572830e7e097a644a04313bd986b
Allexus Technology <ale988240769@gmail.com>
2023-11-27 15:25:58 -0300

    webhook implementado com ia x2

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,68 +1,137 @@
-import clientPromise from "@/libs/mongoConnect";
-import {UserInfo} from "@/models/UserInfo";
-import bcrypt from "bcrypt";
-import * as mongoose from "mongoose";
-import {User} from '@/models/User';
-import NextAuth, {getServerSession, providers} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import { getFirestore } from "firebase/firestore";
+import firebase from "firebase/app";
 
-import { SessionProvider } from 'next-auth/react'
+// Inicialize o Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    // suas configurações do Firebase
+  });
+} else {
+  firebase.app();
+}
 
-import Providers from 'next-auth/providers'
-import { FirebaseAdapter } from '@next-auth/firebase-adapter'
-import firebase from 'firebase/app'
-import 'firebase/firestore';
+const firestore = getFirestore();
 
-const firestore = firebase.firestore()
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: FirebaseAdapter(firestore),
+export default NextAuth({
   providers: [
-    GoogleProvider({
+    Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    CredentialsProvider({
-      name: 'Credentials',
+    Providers.Credentials({
+      name: "Credentials",
       id: 'credentials',
       credentials: {
         username: { label: "Email", type: "email", placeholder: "test@example.com" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
+      async authorize(credentials) {
+        // const email = credentials?.email;
+        // const password = credentials?.password;
 
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({email});
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
+        // mongoose.connect(process.env.MONGO_URL);
+        // const user = await User.findOne({email});
+        // const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-        if (passwordOk) {
-          return user;
-        }
+        // if (passwordOk) {
+        //   return user;
+        // }
 
-        return null
-      }
-    })
+        // return null
+        console.log("ATENTICAÇÃO AUTORIZADA COM SUCESSO!")
+      },
+    }),
   ],
-};
+  adapter: FirebaseAdapter(firestore),
+});
+
 
 export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
+    // const session = await getServerSession(authOptions);
+    // const userEmail = session?.user?.email;
+    // if (!userEmail) {
+    //   return false;
+    // }
+    // const userInfo = await UserInfo.findOne({email:userEmail});
+    // if (!userInfo) {
+    //   return false;
+    // }
+    // return userInfo.admin;
     return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
 }
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST }
+
+
+// import clientPromise from "@/libs/mongoConnect";
+// import {UserInfo} from "@/models/UserInfo";
+// import bcrypt from "bcrypt";
+// import * as mongoose from "mongoose";
+// import {User} from '@/models/User';
+// import NextAuth, {getServerSession, providers} from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
+
+// import { SessionProvider } from 'next-auth/react'
+
+// import Providers from 'next-auth/providers'
+// import { FirebaseAdapter } from '@next-auth/firebase-adapter'
+// import firebase from 'firebase/app'
+// import 'firebase/firestore';
+
+// const firestore = firebase.firestore()
+
+// export const authOptions = {
+//   secret: process.env.SECRET,
+//   adapter: FirebaseAdapter(firestore),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     CredentialsProvider({
+//       name: 'Credentials',
+//       id: 'credentials',
+//       credentials: {
+//         username: { label: "Email", type: "email", placeholder: "test@example.com" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials, req) {
+//         const email = credentials?.email;
+//         const password = credentials?.password;
+
+//         mongoose.connect(process.env.MONGO_URL);
+//         const user = await User.findOne({email});
+//         const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+//         if (passwordOk) {
+//           return user;
+//         }
+
+//         return null
+//       }
+//     })
+//   ],
+// };
+
+// export async function isAdmin() {
+//   const session = await getServerSession(authOptions);
+//   const userEmail = session?.user?.email;
+//   if (!userEmail) {
+//     return false;
+//   }
+//   const userInfo = await UserInfo.findOne({email:userEmail});
+//   if (!userInfo) {
+//     return false;
+//   }
+//   return userInfo.admin;
+// }
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST }
